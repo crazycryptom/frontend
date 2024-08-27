2353f40d4f26004597e81faf4120124e7ac2d47a
crazycryptom <crazycryptom@outlook.com>
2024-08-27 04:46:29 -0400

    first commit

---
+++ src/config/dev.ts
@@ -0,0 +1,8 @@
+export const PROGRAM_ID = 'XYfdh6ta3jhE77QhEFGY8tY1E77rS84Fc2iDfDfafLc';
+// export const CLUSTER_API = 'https://metaplex.devnet.rpcpool.com/';
+// export const CLUSTER_API = 'https://lon43.nodes.rpcpool.com/';
+export const CLUSTER_API = 'https://api.devnet.solana.com';
+export const HOLDER_DATA_SEEDS = 'holder';
+export const REWARD_TOKEN = '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU';
+export const REWARD_TOKEN_DECIMAL = 1000000;
+export const ADMIN_KEY = '3ttYrBAp5D2sTG2gaBjg8EtrZecqBQSBuFRhsqHWPYxX';
---
+++ src/config/main.ts
@@ -0,0 +1,2 @@
+export const PROGRAM_ID = 'XYfdh6ta3jhE77QhEFGY8tY1E77rS84Fc2iDfDfafLc'
+export const CANDY_MACHINE_ID = '8BwH7J5uqqPwFHijjW6mTtyGeButSVNVogYRcjz3urgC';
